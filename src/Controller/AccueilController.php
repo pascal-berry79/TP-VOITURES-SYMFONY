f4ccc0c41994dbb2aabad30acefef9ab5ef46c1c
Pascal Berry <ouanalao.berry@gmail.com>
2020-11-13 12:25:48 +0100

    3th Repository
    Recherche sur la bonne requête dans le AccueilController

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,25 +2,32 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\VoituresRepository;
 use App\Entity\Voitures;
+use App\Repository\ConstructeursRepository;
+use App\Entity\Constructeurs;
+
 
 class AccueilController extends AbstractController
 {
     /**
      * @Route("/", name="accueil")
      */
-    public function index(EntityManagerInterface $em): Response
+    public function index(VoituresRepository $VoituresRepository,ConstructeursRepository $ConstructeursRepository, EntityManagerInterface $em): Response
     {
 
-        $repository=$em->getRepository(Voitures::class);
-        $voitures = $repository -> findAll();
-
+        $voitures = $VoituresRepository->findBy([], ['id'=>'DESC'], 3);
+        // $constructeur = $ConstructeursRepository->find('RAND()');
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'voitures' => $voitures,
+            // 'constructeur' => $constructeur,
         ]);
+    
+
     }
 }
