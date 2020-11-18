44310e77d126e115bc4bbce9ba7ec0167f69ce14
Arthurwwm <68844449+Arthurwwm@users.noreply.github.com>
2020-11-18 13:18:55 +0100

    aller trop loin...

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use App\Repository\AdminRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=AdminRepository::class)
  * @ORM\Table(name="`admin`")
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class Admin implements UserInterface
 {
@@ -39,6 +41,11 @@ class Admin implements UserInterface
      * @ORM\Column(type="string")
      */
     private $password;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
 
     public function __toString(): string
     {
@@ -129,5 +136,17 @@ class Admin implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
     }
 }
