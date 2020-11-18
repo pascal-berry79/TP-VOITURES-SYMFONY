991e9fa10b5026d6637944c59f607f514a42d301
Arthurwwm <68844449+Arthurwwm@users.noreply.github.com>
2020-11-18 13:21:52 +0100

    ?

--- config/bundles.php
+++ config/bundles.php
@@ -8,10 +8,13 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 <<<<<<< HEAD
+<<<<<<< HEAD
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+=======
+>>>>>>> parent of a1c2f9a... ajout easy bundle
 =======
 >>>>>>> parent of a1c2f9a... ajout easy bundle
 ];
